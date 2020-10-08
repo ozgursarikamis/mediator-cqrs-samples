e25f8403da7875a53e77788c7ca787b59a6acf42
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-09 02:50:18 +0300

    Notifications added

---
+++ Mediator.RazorPages/Notifications/NotificationHandler2.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Mediator.RazorPages.Commands;
+using MediatR;
+
+namespace Mediator.RazorPages.Notifications
+{
+    public class NotificationHandler2 : INotificationHandler<AddressAddedNotification>
+    {
+        public Task Handle(AddressAddedNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
