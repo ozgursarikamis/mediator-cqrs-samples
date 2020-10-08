ae8037f8e1cd3c8751a62371b1ae2909690933c6
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-09 02:48:41 +0300

    command added

---
+++ Mediator.RazorPages/Commands/AddAddressCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+namespace Mediator.RazorPages.Commands
+{
+    public class AddAddressCommand : IRequest<AddAddressResponse>
+    {
+        public int UserId { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string StreetAddress { get; set; }
+    }
+
+    public class AddAddressResponse { }
+
+    public class AddressAddedNotification : INotification
+    {
+        public AddressAddedNotification()
+        {
+
+        }
+    }
+
+    #region Notification Handlers
+
+    #endregion
+}
