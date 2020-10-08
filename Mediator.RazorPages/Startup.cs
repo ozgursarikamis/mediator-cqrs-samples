530a286b5fa8960f303741d7f47dee8ac4283508
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-09 02:48:26 +0300

    MediatR packages addedd, Dependency injected

--- Mediator.RazorPages/Startup.cs
+++ Mediator.RazorPages/Startup.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +21,7 @@ namespace Mediator.RazorPages
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
